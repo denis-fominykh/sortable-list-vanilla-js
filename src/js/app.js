fa897902e6879e8cc83cdfba7502c0c79ee5887c
denis-fominykh <fominykhdenis@gmail.com>
2020-02-25 23:53:41 +0200

    creating the drag and drop functionality

--- src/js/app.js
+++ src/js/app.js
@@ -10,6 +10,8 @@ const richestPeople = [
   'Carlos Slim Helu',
   'Larry Page',
   'Mukesh Ambani',
+  'Jeff Bezos',
+  'Bernard Arnault',
 ];
 
 const listItems = [];
@@ -36,5 +38,53 @@ function createList() {
 
       listItems.push(listItem);
       draggableList.appendChild(listItem);
+
+      addEventListeners();
     });
+}
+
+function dragStart() {
+  dragStartIndex = +this.closest('li').getAttribute('data-index');
+}
+
+function dragOver(event) {
+  event.preventDefault();
+}
+
+function dragDrop() {
+  const dragEndIndex = +this.getAttribute('data-index');
+  swapItems(dragStartIndex, dragEndIndex);
+  this.classList.remove('over');
+}
+
+function dragEnter() {
+  this.classList.add('over');
+}
+
+function dragLeave() {
+  this.classList.remove('over');
+}
+
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector('.draggable');
+  const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+}
+
+function addEventListeners() {
+  const draggables = document.querySelectorAll('.draggable');
+  const dragListItems = document.querySelectorAll('.draggable-list li');
+
+  draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', dragStart);
+  });
+
+  dragListItems.forEach(item => {
+    item.addEventListener('dragover', dragOver);
+    item.addEventListener('drop', dragDrop);
+    item.addEventListener('dragenter', dragEnter);
+    item.addEventListener('dragleave', dragLeave);
+  });
 }
